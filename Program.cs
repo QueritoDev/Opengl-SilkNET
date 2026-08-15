c82028200cd7473f8d0544690e62b5c02fd92230
QueritoDEV <dazzlefps11@gmail.com>
2026-08-14 23:17:36 -0300

    Some code improvements

--- Program.cs
+++ Program.cs
@@ -3,14 +3,30 @@ using Silk.NET.Maths;
 using Silk.NET.Windowing;
 using Silk.NET.OpenGL;
 using System.Drawing;
+
 public class Program
 {
-    private static uint _program;
-    private static uint _vbo;
-    private static uint _vao;
-    private static uint _ebo;
-    private static GL _gl;
     private static IWindow _window;
+    private static GL _gl;
+    private static uint _vbo;
+    private static uint _ebo;
+    private static uint _vao;
+    private static uint _Shader;
+    
+    private static readonly float[] Vertices =
+    {
+        0.0f, 0.5f, 0.0f, // 1st vertice
+        -0.5f, -0.5f, 0.0f, // 2nd vertice 
+        0.5f, -0.5f, 0.0f // 3rd vertice
+    };
+        
+    private static readonly uint[] Indices =
+    {
+        0u, 1u, 3u,
+        1u, 2u, 3u,
+    };
+        
+
     public static void Main(string[] args)
     {
         WindowOptions options = WindowOptions.Default with
@@ -20,46 +36,43 @@ public class Program
         };
 
         _window = Window.Create(options);
+        
         _window.Load += OnLoad;
         
         _window.Update += OnUpdate;
         _window.Render += OnRender;
+        _window.Update += OnUpdate;
 
+        _window.Closing += OnClose;
         _window.Run();
+        
+
         _window.Dispose();
     }
 
     public static unsafe void OnLoad()
     {
+        IInputContext input = _window.CreateInput();
+        for(int i = 0; i < input.Keyboards.Count; i++)
+            input.Keyboards[i].KeyDown += KeyDown;
         _gl = _window.CreateOpenGL();
+        
         _gl.ClearColor(Color.Wheat);
         _vao = _gl.GenVertexArray();
         _gl.BindVertexArray(_vao);
         _vbo = _gl.GenBuffer();
         _gl.BindBuffer(BufferTargetARB.ArrayBuffer, _vbo);
-        float[] vertices =
-        {
-            0.5f,  0.5f, 0.0f,
-            0.5f, -0.5f, 0.0f,
-            -0.5f, -0.5f, 0.0f,
-            -0.5f,  0.5f, 0.0f
-        };
         
-        uint[] indices =
-        {
-            0u, 1u, 3u,
-            1u, 2u, 3u
-        };
         
         _ebo = _gl.GenBuffer();
         _gl.BindBuffer(BufferTargetARB.ElementArrayBuffer, _ebo);
 
-        fixed (float* buf = vertices)
-            _gl.BufferData(BufferTargetARB.ArrayBuffer, (nuint) (vertices.Length * sizeof(float)), buf, BufferUsageARB.StaticDraw);
+        fixed (float* buf = Vertices)
+            _gl.BufferData(BufferTargetARB.ArrayBuffer, (nuint) (Vertices.Length * sizeof(float)), buf, BufferUsageARB.StaticDraw);
         Console.WriteLine("Load!");
 
-        fixed (uint* buf = indices)
-            _gl.BufferData(BufferTargetARB.ElementArrayBuffer, (nuint) (indices.Length * sizeof(uint)), buf, BufferUsageARB.StaticDraw);
+        fixed (uint* buf = Indices)
+            _gl.BufferData(BufferTargetARB.ElementArrayBuffer, (nuint) (Indices.Length * sizeof(uint)), buf, BufferUsageARB.StaticDraw);
 
         // CREATE THE VERTEX CODE
         const string vertexCode = @"
@@ -94,8 +107,6 @@ public class Program
         if (vStatus != (int) GLEnum.True) //CASO O VERTEX SHADER FALHE MANDA LOG
             throw new Exception("Vertex shader failed to compile: " + _gl.GetShaderInfoLog(vertexShader));
 
-
-        
         uint fragmentShader = _gl.CreateShader(ShaderType.FragmentShader);
         _gl.ShaderSource(fragmentShader, fragmentCode);
         _gl.CompileShader(fragmentShader);
@@ -104,29 +115,28 @@ public class Program
         if(fStatus != (int) GLEnum.True) 
             throw new Exception("Fragment shader failed to compile: " + _gl.GetShaderInfoLog(fragmentShader));
 
-
         /* CREATE THE PROGRAM
         LOGICS SEQUENCE:
             - CREATE PROGRAM
             - ATTACH SHADERS
             - LINK THEM
             - GET INFO FROM PROGRAM TO DEBUG
-            - DETACH SHADERS (_program, {SHADER})
+            - DETACH SHADERS (_Shader, {SHADER})
             - DELETE SHADERS
         */
-        _program = _gl.CreateProgram();
-        _gl.AttachShader(_program, vertexShader); //ATTACH SHADER TO THE PROGRAM
-        _gl.AttachShader(_program, fragmentShader); //ATTACH SHADER TO THE PROGRAM
+        _Shader = _gl.CreateProgram();
+        _gl.AttachShader(_Shader, vertexShader); //ATTACH SHADER TO THE PROGRAM
+        _gl.AttachShader(_Shader, fragmentShader); //ATTACH SHADER TO THE PROGRAM
 
-        _gl.LinkProgram(_program); //LINKS VERTEX AND FRAGMENT SHADERS INTO A SINGLE EXECUTABLE PROGRAM READY FOR GPU RENDERING
+        _gl.LinkProgram(_Shader); //LINKS VERTEX AND FRAGMENT SHADERS INTO A SINGLE EXECUTABLE PROGRAM READY FOR GPU RENDERING
 
-        _gl.GetProgram(_program, ProgramPropertyARB.LinkStatus, out int lStatus);
+        _gl.GetProgram(_Shader, ProgramPropertyARB.LinkStatus, out int lStatus);
         
         if (lStatus != (int) GLEnum.True)
-            throw new Exception("Program failed to link: " + _gl.GetProgramInfoLog(_program));
+            throw new Exception("Program failed to link: " + _gl.GetProgramInfoLog(_Shader));
 
-        _gl.DetachShader(_program, vertexShader);
-        _gl.DetachShader(_program, fragmentShader);
+        _gl.DetachShader(_Shader, vertexShader);
+        _gl.DetachShader(_Shader, fragmentShader);
         _gl.DeleteShader(vertexShader);
         _gl.DeleteShader(fragmentShader);
 
@@ -138,10 +148,6 @@ public class Program
         _gl.BindBuffer(BufferTargetARB.ArrayBuffer, 0);
         _gl.BindBuffer(BufferTargetARB.ElementArrayBuffer, 0);
 
-        IInputContext input = _window.CreateInput();
-        for(int i = 0; i < input.Keyboards.Count; i++)
-            input.Keyboards[i].KeyDown += KeyDown;
-        
     }
 
 
@@ -154,9 +160,8 @@ public class Program
     {
         _gl.Clear(ClearBufferMask.ColorBufferBit);
         _gl.BindVertexArray(_vao);
-        _gl.UseProgram(_program);
-        _gl.DrawElements(PrimitiveType.Triangles, 6, DrawElementsType.UnsignedInt, (void*)0);
-
+        _gl.UseProgram(_Shader);
+        _gl.DrawElements(PrimitiveType.Triangles, (uint) Indices.Length, DrawElementsType.UnsignedInt, (void*)0);
     }
 
     private static void KeyDown(IKeyboard keyboard, Key key, int keyCode)
@@ -164,5 +169,13 @@ public class Program
         Console.WriteLine(key);
         if (key == Key.Escape)
             _window.Close();
+    }
+
+    private static void OnClose()
+    {
+        //Delete buffers
+        _gl.DeleteBuffer(_vbo);
+        _gl.DeleteBuffer(_ebo);
+        _gl.DeleteProgram(_Shader);
     }
 }
